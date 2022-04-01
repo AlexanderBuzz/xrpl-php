58d683f9bb50bd3a093930fbe8b97e7eea1ec1b1
AlexanderBusse <a.busse@reply.de>
2022-04-01 09:31:59 +0200

    Added phpunit

---
+++ tests/PhpUnitTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class PhpUnitTest extends TestCase
+{
+    public function testInitial(): void
+    {
+        $this->assertEquals(
+            true,
+            true
+        );
+    }
+
+}
