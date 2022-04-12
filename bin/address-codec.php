081904370e48be5761ae553c08175e62f9c75391
AlexanderBusse <a.busse@reply.de>
2022-04-12 17:57:29 +0200

    Fixed example output readability

--- bin/address-codec.php
+++ bin/address-codec.php
@@ -9,11 +9,11 @@ $codec = new RippleAddressCodec();
 
 //Should be: 'XVLhHMPHU98es4dbozjVtdWzVrDjtV18pX8yuPT7y4xaEHi'
 $encoded = $codec->classicAddressToXAddress('rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf', 4294967295);
-print_r("Encoded: " .$encoded);
+print_r("Encoded Address: " . $encoded . PHP_EOL );
 
 //should be: 'T7oKJ3q7s94kDH6tpkBowhetT1JKfcfdSCmAXbS75iATyLD'
 $encoded = $codec->classicAddressToXAddress('r3SVzk8ApofDJuVBPKdmbbLjWGCCXpBQ2g', 123, true);
-print_r("Testnet Encoded: " .$encoded);
+print_r("Testnet Encoded Address: " . $encoded . PHP_EOL );
 
 
 $decoded = $codec->xAddressToClassicAddress('XVLhHMPHU98es4dbozjVtdWzVrDjtV18pX8yuPT7y4xaEHi');
@@ -23,8 +23,10 @@ $decoded = $codec->xAddressToClassicAddress('XVLhHMPHU98es4dbozjVtdWzVrDjtV18pX8
 //  "tag" => 4294967295,
 //  "test" => false
 //[
+print_r("Decoded Address:");
 print_r($decoded);
+print_r(PHP_EOL );
 
 $valid = $codec->isValidXAddress('XVLhHMPHU98es4dbozjVtdWzVrDjtV18pX8yuPT7y4xaEHi');
 //should be: 1
-print_r($valid);
+print_r("Test Address validity: " . $valid . PHP_EOL);
