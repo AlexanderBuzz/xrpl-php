e8749550e91f621f94aed618b4a370ece68581d7
AlexanderBuzz <alexander.busse@posteo.net>
2022-10-13 16:28:59 +0200

    Added Utils/XrpConversion

---
+++ src/Utils/XrpConversion.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace XRPL_PHP\Utils;
+
+use Brick\Math\BigDecimal;
+use Brick\Math\BigInteger;
+use Brick\Math\BigNumber;
+use Brick\Math\RoundingMode;
+use Exception;
+
+const DROPS_PER_XRP = 1000000.0;
+const MAX_FRACTION_LENGTH = 6;
+const SANITY_CHECK = "/^-?[0-9.]+$/u";
+
+if (! function_exists('XRPL_PHP\Utils\dropsToXrp')) {
+    /**
+     * Create a new queued Closure event listener.
+     *
+     * @param string $dropsToConvert
+     * @return string
+     * @throws Exception
+     */
+    function dropsToXrp(mixed $dropsToConvert): string
+    {
+        $drops = (string) BigInteger::of($dropsToConvert);
+
+        if (str_contains($drops, '.')) {
+            throw new Exception("dropsToXrp: value \"{$drops}\" has too many decimal places.");
+        }
+
+        if (!preg_match(SANITY_CHECK, $drops)) {
+            throw new Exception("dropsToXrp: failed sanity check - value \"{$drops}\" does not match (^-?[0-9]+$).");
+        }
+
+        return (string) BigDecimal::of($drops)->exactlyDividedBy(DROPS_PER_XRP);
+    }
+}
+
+if (! function_exists('XRPL_PHP\Utils\xrpToDrops')) {
+    /**
+     * Create a new queued Closure event listener.
+     *
+     * @param  string $xrpToconvert
+     * @return string
+     */
+    function xrpToDrops(string $xrpToConvert): string
+    {
+        $xrp = (string) BigDecimal::of($xrpToConvert);
+
+        if (!preg_match(SANITY_CHECK, $xrp)) {
+            throw new Exception("xrpToDrops: failed sanity check - value \"{$xrp}\" does not match (^-?[0-9]+$).");
+        }
+
+        $components = explode('.', $xrp);
+        if(count($components) > 2) {
+            throw new Exception("xrpToDrops: failed sanity check - value \"{$xrp}\" has too many decimal points.");
+        }
+
+        if(isset($components[1]) && strlen(rtrim($components[1], '0')) > MAX_FRACTION_LENGTH) {
+            throw new Exception("xrpToDrops: value  \"{$xrp}\" has too many decimal places.");
+        }
+
+        return BigDecimal::of($xrp)->multipliedBy(DROPS_PER_XRP)->toBigInteger();
+    }
+}
---
+++ tests/Utils/XrpConversionTest.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Utils;
+
+use PHPUnit\Framework\TestCase;
+use function XRPL_PHP\Utils\dropsToXrp;
+use function XRPL_PHP\Utils\xrpToDrops;
+
+/**
+ * https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/test/utils/dropsToXrp.ts
+ * https://github.com/XRPLF/xrpl.js/blob/main/packages/xrpl/test/utils/xrpToDrops.ts
+ */
+
+class XrpConversionTest  extends TestCase
+{
+    public function testDropsToXrp(): void
+    {
+        //Test typical amount
+        $this->assertEquals(
+            '2',
+            dropsToXrp('2000000')
+        );
+
+        //Test fractions
+        $this->assertEquals(
+            '3.456789',
+            dropsToXrp('3456789')
+        );
+
+        $this->assertEquals(
+            '3.4',
+            dropsToXrp('3400000')
+        );
+
+        $this->assertEquals(
+            '0.000001',
+            dropsToXrp('1')
+        );
+
+        $this->assertEquals(
+            '0.000001',
+            dropsToXrp('1')
+        );
+
+        $this->assertEquals(
+            '0.000001',
+            dropsToXrp('1.00')
+        );
+
+        //Test zero
+        $this->assertEquals(
+            '0',
+            dropsToXrp('0')
+        );
+
+        $this->assertEquals(
+            '0',
+            dropsToXrp('-0')
+        );
+
+        $this->assertEquals(
+            '0',
+            dropsToXrp('0.00')
+        );
+
+        $this->assertEquals(
+            '0',
+            dropsToXrp('00000000')
+        );
+
+        //Test negative values
+        $this->assertEquals(
+            '-2',
+            dropsToXrp('-2000000')
+        );
+
+        //Test decimal points
+        $this->assertEquals(
+            '2',
+            dropsToXrp('2000000.')
+        );
+
+        $this->assertEquals(
+            '-2',
+            dropsToXrp('-2000000.')
+        );
+
+        //Test BigNumber
+
+        //Test number/int
+
+        //Test scientific notation
+    }
+
+    public function testXrpToDrops(): void
+    {
+        //Test typical amount
+        $this->assertEquals(
+            '2000000',
+            xrpToDrops('2')
+        );
+
+        //Test fractions
+        $this->assertEquals(
+            '3456789',
+            xrpToDrops('3.456789')
+        );
+
+        $this->assertEquals(
+            '3400000',
+            xrpToDrops('3.400000')
+        );
+
+        $this->assertEquals(
+            '1',
+            xrpToDrops('0.000001')
+        );
+
+        $this->assertEquals(
+            '1',
+            xrpToDrops('0.0000010')
+        );
+
+        //Test zero
+        $this->assertEquals(
+            '0',
+            xrpToDrops('0')
+        );
+
+        $this->assertEquals(
+            '0',
+            xrpToDrops('-0')
+        );
+
+        $this->assertEquals(
+            '0',
+            xrpToDrops('0.000000')
+        );
+
+        $this->assertEquals(
+            '0',
+            xrpToDrops('0.0000000')
+        );
+
+        //Test negative values
+        $this->assertEquals(
+            '-2000000',
+            xrpToDrops('-2')
+        );
+
+        //Test "works with a value ending with a decimal point"
+        $this->assertEquals(
+            '2000000',
+            xrpToDrops('2.')
+        );
+
+        $this->assertEquals(
+            '-2000000',
+            xrpToDrops('-2.')
+        );
+
+        //Test BigNumber
+
+        //Test number/int
+
+        //Test scientific notation
+
+    }
+}
