70b0e5d4961be1dfb49f5a4fb4686b73ea9ef7a5
AlexanderBuzz <alexander.busse@posteo.net>
2022-11-14 16:14:39 +0100

    - Added NFT example [WIP]

--- examples/quickstart/3.mint-nfts.php
+++ examples/quickstart/3.mint-nfts.php
@@ -1,1 +1,32 @@
 <?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . './_const.php';
+
+use XRPL_PHP\Core\Buffer;
+use XRPL_PHP\Client\JsonRpcClient;
+
+function convertStringToHex(string $in): string {
+    $hex = '';
+    foreach(str_split($in) as $char) {
+        $hex .= dechex(ord($char));
+    }
+
+    return strtoupper($hex);
+}
+
+$client = new JsonRpcClient(RPC_NFT_DEVNET_URL);
+$standbyWallet = $client->fundWallet($client);
+
+$standbyTokenUrl = '';
+$standbyFlags = '';
+$standbyTransferFee = '';
+
+$transactionBlob = [
+    "TransactionType" => "NFTokenMint",
+    "Account" => $standbyWallet->getClassicAddress(),
+    "URI" => convertStringToHex($standbyTokenUrl),
+    "Flags" => (int) $standbyFlags,
+    "TransferFee" => (int) $standbyTransferFee,
+    "NFTokenTaxon" >= 0 //Required, but if you have no use for it, set to zero.
+];
---
+++ examples/quickstart/_const.php
@@ -0,0 +1,20 @@
+<?php
+
+//Testnet
+const WS_TESTNET_URL = 'wss://s.altnet.rippletest.net';
+const RPC_TESTNET_URL = 'https://s.altnet.rippletest.net:51234';
+const RPC_TESTNET_FAUCET_URL = 'https://s.altnet.rippletest.net:51234';
+
+//Devnet
+const WS_DEVNET_URL = 'wss://s.devnet.rippletest.net';
+const RPC_DEVNET_URL = 'https://s.devnet.rippletest.net:51234';
+const RPC_DEVNET_FAUCET_URL = '';
+
+//NFT-Devnet
+const WS_NFT_DEVNET_URL = 'wss://xls20-sandbox.rippletest.net:51233';
+const RPC_NFT_DEVNET_URL = 'https://xls20-sandbox.rippletest.net:51234';
+const RPC_NFT_DEVNET_FAUCET_URL = '';
+
+//TODO: Add Hooks V1
+
+//TODO: Add Hooks V2
